6c0d48b23635f2beacf8e429657b88a11c97bf0b
canrnd <canrnd@outlook.com>
2024-05-26 04:21:52 -0600

    add main.css, add to main.js and formater fixes

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     loadHTML('/nav.html', 'nav-placeholder');
     //loadHTML('/head.html', 'head-placeholder');
     loadHTML('/footer.html', 'footer-placeholder');
-    
+
 });
 
 function myAccFunc(section) {
@@ -32,4 +32,86 @@ function w3_open() {
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
+};
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var images = document.getElementsByClassName('imgZoom');
+var imageIndex;
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = "none";
+};
+
+// Attach event listener to the window to close modal on clicking outside of the image or buttons
+window.onclick = function (event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+};
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = "flex";
+};
+
+// Function to set the image based on the imageIndex
+function setCurrentImage() {
+    modalImg.src = images[imageIndex].src;
+    modalImg.alt = images[imageIndex].alt;
+};
+
+// Function to change the image
+function changeImage(step) {
+    imageIndex += step;
+    if (imageIndex >= images.length) {
+        imageIndex = 0;
+    } else if (imageIndex < 0) {
+        imageIndex = images.length - 1;
+    }
+    setCurrentImage();
+};
+
+// Attach event listeners to images to open modal
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function () {
+        imageIndex = Array.prototype.indexOf.call(images, this);
+        openModal();
+        setCurrentImage();
+    }
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+};
+
+function copyToClipboard(element) {
+    const textToCopy = element.textContent; // Get the text content of the xmp element
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            showTooltip(element, 'Copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Failed to copy text: ', err);
+            showTooltip(element, 'Failed to copy!');
+        });
+};
+
+function showTooltip(element, message) {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.textContent = message;
+    document.body.appendChild(tooltip);
+    const coords = element.getBoundingClientRect();
+    tooltip.style.left = `${coords.left + window.scrollX}px`;
+    tooltip.style.top = `${coords.top + coords.height + window.scrollY + 5}px`;
+    tooltip.style.opacity = 1;
+
+    setTimeout(() => {
+        tooltip.style.opacity = 0;
+        setTimeout(() => document.body.removeChild(tooltip), 500);
+    }, 3000); // Display for 3 seconds
 };
